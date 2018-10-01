2b4ca142430724d4842b30acc2c4bbc4eb094540
flameAngular <43732045+flameAngular@users.noreply.github.com>
2018-10-01 17:54:23 +0530

    Create Hello.java

---
+++ scripts/Hello.java
@@ -0,0 +1,11 @@
+// LANGUAGE: JAVA
+// ENV: JVM
+// AUTHOR: Sandeep Ranjan
+// GITHUB: https://github.com/flameAngular
+
+class Hello {
+  public static void main(String[] args) {
+    System.out.println("Hello World");
+  }
+}
+   
